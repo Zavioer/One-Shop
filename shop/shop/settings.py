0ea51f28485f958b6db58a576d59ab64bfff8334
Zavioer <piotrekq321@gmail.com>
2023-01-12 01:22:12 +0100

    change db to sqlite

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
     }
 }
 
